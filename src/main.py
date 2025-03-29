d382f802650719784414cf31384e05fc3c2dc508
Roobika-T <roobikatura@gmail.com>
2025-03-29 19:39:08 +0530

    optimized code for collision handling

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from gui.fleetmanagement import FleetManagementApp
 import tkinter as tk
 import logging 
+import os
 
 logging.basicConfig(
     filename='src/logs/fleet_logs.txt',
@@ -15,17 +16,14 @@ def main(json_path: str, spawn_prefix: str = "m"):
     root.mainloop()
 
 if __name__ == "__main__":
-    import os
 
     json_files = [
         'nav_graph_1.json',
         'nav_graph_2.json',
         'nav_graph_3.json'
     ]
-    print(os.getcwd())
     base_path = 'data/nav_graph_samples'
-    selected_file = json_files[1]
-    print(selected_file)
+    selected_file = json_files[0]
     json_path = os.path.join(base_path, selected_file)
 
     spawn_prefix = "m" if "nav_graph_1" in selected_file or "nav_graph_2" in selected_file else "p"
